2ff430fae75ed1d3a10277bb3ff121a8338abad8
Aman-ABJIMA <aman.singh@abjima.com>
2022-12-20 18:29:13 +0530

    Add project files.

---
+++ LINQSamples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LINQSamples
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Instantiate the Samples ViewModel
+      SamplesViewModel vm = new SamplesViewModel
+      {
+        // Use Query or Method Syntax?
+        UseQuerySyntax = true
+      };
+
+            // Call a sample method     
+            //vm.GetAllLooping();      vm.GetAll();
+            // vm.GetSingleColumn();   vm.GetSpecificColumns();
+            // vm.AnonymousClass();
+            // vm.OrderBy();
+            //  vm.OrderByDescending();
+            // vm.OrderByTwoFields();
+            vm.WhereExpression();
+
+      // Display Product Collection
+      foreach (var item in vm.Products) {
+        Console.Write(item.ToString());
+      }
+
+      // Display Result Text
+      Console.WriteLine(vm.ResultText);
+    }
+  }
+}
